d3763ca0f0324294688691254911c417c2a65542
Din41296 <dina.muasher@icloud.com>
2021-06-23 14:01:52 +0300

    editings

--- js/app.js
+++ js/app.js
@@ -3,16 +3,16 @@
 let count = 0;
 let q1 = prompt('Do I like Black color?(yes/no)');
 switch (q1.toLowerCase()) {
-case 'yes':
-case 'y':
-  console.log('You\'re right');
-  count++;
-  alert('You\'re right');
-  break;
-default:
-  console.log('wrong answer');
-  alert('wrong answer');
-  break;
+  case 'yes':
+  case 'y':
+    console.log('You\'re right');
+    count++;
+    alert('You\'re right');
+    break;
+  default:
+    console.log('wrong answer');
+    alert('wrong answer');
+    break;
 
 }
 
@@ -36,21 +36,21 @@ else if (q2 === 'y') {
 
 let q3 = prompt('Do I like healthy food?(YES/NO)').toUpperCase();
 switch (q3) {
-case 'YES':
-case 'Y':
-  console.log('Absolutely wrong');
-  alert('Absolutely wrong');
-  break;
-case 'NO':
-case 'N':
-  console.log('you\'re right');
-  count++;
-  alert('you\'re right');
-  break;
-default:
-  console.log('Please next time answer yes/no');
-  alert('Please next time answer yes/no');
-  break;
+  case 'YES':
+  case 'Y':
+    console.log('Absolutely wrong');
+    alert('Absolutely wrong');
+    break;
+  case 'NO':
+  case 'N':
+    console.log('you\'re right');
+    count++;
+    alert('you\'re right');
+    break;
+  default:
+    console.log('Please next time answer yes/no');
+    alert('Please next time answer yes/no');
+    break;
 }
 
 let q4 = prompt('Do I have a driving license?(YES/NO)').toUpperCase();
@@ -75,41 +75,41 @@ if (q4 === 'YES') {
 
 let q5 = prompt('Do I watch turkish series?(yes/no)').toLowerCase();
 switch (q5) {
-case 'yes':
-case 'y':
-  console.log('Right');
-  count++;
-  alert('Right');
-  break;
-case 'no':
-case 'n':
-  console.log('Wrong');
-  alert('Wrong');
-  break;
-default:
-  console.log('Follow the question requirements');
-  alert('Follow the question requirement');
-  break;
+  case 'yes':
+  case 'y':
+    console.log('Right');
+    count++;
+    alert('Right');
+    break;
+  case 'no':
+  case 'n':
+    console.log('Wrong');
+    alert('Wrong');
+    break;
+  default:
+    console.log('Follow the question requirements');
+    alert('Follow the question requirement');
+    break;
 
 }
 
 let q6 = parseFloat(prompt('What do you think about my lucky number?'));
-for(let i=0;i<4;i++){
-  if(q6 === 50){
+for (let i = 0; i < 4; i++) {
+  if (q6 === 50) {
     alert('you\'re right');
     console.log(i);
     count++;
     console.log(q6);
     break;
 
-  } else if(q6>50){
+  } else if (q6 > 50) {
     alert('you\'re wrong too high answer');
     console.log(i);
     console.log(q6);
     q6 = parseFloat(prompt('What do you think about my lucky number?'));
 
 
-  } else if(q6<50){
+  } else if (q6 < 50) {
     alert('you\'re wrong too low answer');
     console.log(i);
     console.log(q6);
@@ -125,32 +125,23 @@ alert('My lucky number is 50');
 
 
 let q7 = parseFloat(prompt('Guess my favorite 3 favorite numbers?'));
-let numbers = [5,3,10];
-for(let i=0;i<=5;i++){
-  if(q7 === numbers[i]){
-    console.log(i);
-    alert('You\'re right');
-    count++;
-    break;
-  } else if(q7 !== numbers[i]) {
-    q7 = parseFloat(prompt('Guess my favorite 3 favorite numbers?'));
-    console.log(q7);
+let numbers = [5, 3, 10];
+for (let att = 0; att <= 5; att++) {
+  for (let i = 0; i < numbers.length; i++) {
+    if (q7 === numbers[i]) {
+      alert('correct');
+      att = 6;
+
+    }
 
   }
 }
 
-alert('My favorite 3 numbers is =  ' + numbers);
 
 
 
 alert('Your score is = ' + count + 'of 7');
 
-
-let userName = prompt('Kindly enter you name');
-console.log('Welcome to my website ' + userName);
-alert('Welcome to my website ' + userName);
-console.log('Your username is : ' + userName + '123');
-alert('Your username is : ' + userName + '123');
 
 
 
