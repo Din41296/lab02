ea400d4555c09c0a7e5616b4950299b711ab3920
Din41296 <dina.muasher@icloud.com>
2021-06-25 15:31:44 +0300

    editing

--- js/app.js
+++ js/app.js
@@ -1,86 +1,53 @@
 'use strict';
 
 let count = 0;
-<<<<<<< HEAD
-let q1 = prompt('Do I like Black color?(yes/no)');
-switch (q1.toLowerCase()) {
-case 'yes':
-case 'y':
-  console.log('You\'re right');
-  count++;
-  alert('You\'re right');
-  break;
-default:
-  console.log('wrong answer');
-  alert('wrong answer');
-  break;
-=======
 let q1 =  function(){
 
-   q1= prompt('Do I like Black color?(yes/no)');
+  q1= prompt('Do I like Black color?(yes/no)');
   switch (q1.toLowerCase()) {
-    case 'yes':
-    case 'y':
-      console.log('You\'re right');
-      count++;
-      alert('You\'re right');
-      break;
-    default:
-      console.log('wrong answer');
-      alert('wrong answer');
-      break;
-    
-  
+  case 'yes':
+  case 'y':
+    console.log('You\'re right');
+    count++;
+    alert('You\'re right');
+    break;
+  default:
+    console.log('wrong answer');
+    alert('wrong answer');
+    break;
+
+
   }
   return q1;
 } ;
 q1();
->>>>>>> 06e31b7262266a7dc851d033bd1c0b2fe6cdee60
 
 
 
 
 
 
-<<<<<<< HEAD
-let q3 = prompt('Do I like healthy food?(YES/NO)').toUpperCase();
-switch (q3) {
-case 'YES':
-case 'Y':
-  console.log('Absolutely wrong');
-  alert('Absolutely wrong');
-  break;
-case 'NO':
-case 'N':
-  console.log('you\'re right');
-  count++;
-  alert('you\'re right');
-  break;
-default:
-  console.log('Please next time answer yes/no');
-  alert('Please next time answer yes/no');
-  break;
-}
-=======
+
+
 let q2 = function(){
->>>>>>> 06e31b7262266a7dc851d033bd1c0b2fe6cdee60
+
 
   q2= prompt('Do I like football?(yes/no)').toLowerCase();
- 
- if (q2 === 'yes') {
-   console.log('Right');
-   count++;
-   alert('Right');
- }
- else if (q2 === 'y') {
-   console.log('Right');
-   count++;
-   alert('Right');
- } else {
-   console.log('wrong');
-   alert('wrong');
- }
- return q2;
+
+  if (q2 === 'yes') {
+    console.log('Right');
+    count++;
+    alert('Right');
+  }
+  else if (q2 === 'y') {
+    console.log('Right');
+    count++;
+    alert('Right');
+  } else {
+    console.log('wrong');
+    alert('wrong');
+  }
+  return q2;
 };
 q2();
 
@@ -91,25 +58,25 @@ q2();
 
 let q3 = function(){
 
-   q3= prompt('Do I like healthy food?(YES/NO)').toUpperCase();
- switch (q3) {
-   case 'YES':
-   case 'Y':
-     console.log('Absolutely wrong');
-     alert('Absolutely wrong');
-     break;
-   case 'NO':
-   case 'N':
-     console.log('you\'re right');
-     count++;
-     alert('you\'re right');
-     break;
-   default:
-     console.log('Please next time answer yes/no');
-     alert('Please next time answer yes/no');
-     break;
- }
-return q3;
+  q3= prompt('Do I like healthy food?(YES/NO)').toUpperCase();
+  switch (q3) {
+  case 'YES':
+  case 'Y':
+    console.log('Absolutely wrong');
+    alert('Absolutely wrong');
+    break;
+  case 'NO':
+  case 'N':
+    console.log('you\'re right');
+    count++;
+    alert('you\'re right');
+    break;
+  default:
+    console.log('Please next time answer yes/no');
+    alert('Please next time answer yes/no');
+    break;
+  }
+  return q3;
 
 } ;
 q3();
@@ -123,26 +90,7 @@ q3();
 
 let q4 = function(){
 
-<<<<<<< HEAD
-let q5 = prompt('Do I watch turkish series?(yes/no)').toLowerCase();
-switch (q5) {
-case 'yes':
-case 'y':
-  console.log('Right');
-  count++;
-  alert('Right');
-  break;
-case 'no':
-case 'n':
-  console.log('Wrong');
-  alert('Wrong');
-  break;
-default:
-  console.log('Follow the question requirements');
-  alert('Follow the question requirement');
-  break;
-=======
-   q4= prompt('Do I have a driving license?(YES/NO)').toUpperCase();
+  q4= prompt('Do I have a driving license?(YES/NO)').toUpperCase();
   if (q4 === 'YES') {
     console.log('I\'ll take you for drive one day');
     count++;
@@ -161,7 +109,7 @@ default:
     console.log('Kindly follow the question requirements');
     alert('Kindly follow the question requirements');
   }
-return q4;
+  return q4;
 };
 q4();
 
@@ -174,61 +122,60 @@ q4();
 let q5 =  function(){
 
   q5= prompt('Do I watch turkish series?(yes/no)').toLowerCase();
- switch (q5) {
-   case 'yes':
-   case 'y':
-     console.log('Right');
-     count++;
-     alert('Right');
-     break;
-   case 'no':
-   case 'n':
-     console.log('Wrong');
-     alert('Wrong');
-     break;
-   default:
-     console.log('Follow the question requirements');
-     alert('Follow the question requirement');
-     break;
- 
- }
-return q5;
+  switch (q5) {
+  case 'yes':
+  case 'y':
+    console.log('Right');
+    count++;
+    alert('Right');
+    break;
+  case 'no':
+  case 'n':
+    console.log('Wrong');
+    alert('Wrong');
+    break;
+  default:
+    console.log('Follow the question requirements');
+    alert('Follow the question requirement');
+    break;
+
+  }
+  return q5;
 };
 q5();
 
->>>>>>> 06e31b7262266a7dc851d033bd1c0b2fe6cdee60
 
 
 
 let q6 = function(){
 
   q6= parseFloat(prompt('What do you think about my lucky number?'));
-for (let i = 0; i < 4; i++) {
- if (q6 === 50) {
-   alert('you\'re right');
-   console.log(i);
-   count++;
-   console.log(q6);
-   break;
+  for (let i = 0; i < 4; i++) {
+    if (q6 === 50) {
+      alert('you\'re right');
+      console.log(i);
+      count++;
+      console.log(q6);
+      break;
 
- } else if (q6 > 50) {
-   alert('you\'re wrong too high answer');
-   console.log(i);
-   console.log(q6);
-   q6 = parseFloat(prompt('What do you think about my lucky number?'));
-
-
- } else if (q6 < 50) {
-   alert('you\'re wrong too low answer');
-   console.log(i);
-   console.log(q6);
-   q6 = parseFloat(prompt('What do you think about my lucky number?'));
- }
+    } else if (q6 > 50) {
+      alert('you\'re wrong too high answer');
+      console.log(i);
+      console.log(q6);
+      q6 = parseFloat(prompt('What do you think about my lucky number?'));
 
 
+    } else if (q6 < 50) {
+      alert('you\'re wrong too low answer');
+      console.log(i);
+      console.log(q6);
+      q6 = parseFloat(prompt('What do you think about my lucky number?'));
+    }
 
-}
-return q6;
+
+
+  }
+  return q6;
 };
 q6();
 
@@ -247,9 +194,9 @@ let q7 = function(){
       if (q7 === numbers[i]) {
         alert('correct');
         att = 6;
-  
+
       }
-  
+
     }
   }
   return q7;
@@ -268,7 +215,7 @@ alert('Your score is = ' + count + 'of 7');
 
 
 
-if(cookieCount *= 5);
+
 
 
 
